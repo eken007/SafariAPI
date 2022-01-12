cf4edb872d982c9e5ef1beceee5e0422574754f3
eken007 <ekenfranky@gmail.com>
2022-01-12 23:16:38 +0100

    new 9 commit

--- app/Http/Controllers/ConfirmationMailController.php
+++ app/Http/Controllers/ConfirmationMailController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Mail\ConfirmeMail;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class ConfirmationMailController extends Controller
 {
-    public function sendEmail(){
+    public function sendEmail(Request $request){
+        $users= User::all();
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+        ]);
+
+        if ($validator->fails())
+        {
+            $response = ["message" => "Mail incorrect"];
+            return response(['errors'=>$validator->errors()->all(), $response], 422);
+        }
+        
         $details = [
             'title' => 'Votre code de vérification pour finaliser votre inscription est le :',
             'body' => request('code')
